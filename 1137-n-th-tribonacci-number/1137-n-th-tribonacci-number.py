637fab195646f38b475cd461991007c2af093141
Hanshitha Dhavala <hanshitharoshan@gmail.com>
2026-08-29 11:10:46 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        F = [0]*38 
+        F[0] = 0
+        F[1] = 1
+        F[2] = 1
+        for i in range(n-2):
+            F[i+3] = F[i] + F[i+1] + F[i+2]
+        return F[n]
+
+
+        
