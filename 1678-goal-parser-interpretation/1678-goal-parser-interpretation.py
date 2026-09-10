8f6652ae388422e1d17b7d3cbfb172a0f3a484ce
Hanshitha Dhavala <hanshitharoshan@gmail.com>
2026-09-10 10:50:34 +0530

    Time: 47 ms (43.37%), Space: 19.2 MB (43.94%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        res = []
+        i = 0
+        n = len(command)
+        
+        while i < n:
+            if command[i] == 'G':
+                res.append('G')
+                i += 1
+            elif command[i:i+2] == '()':
+                res.append('o')
+                i += 2
+            else: 
+                res.append('al')
+                i += 4
+                
+        return "".join(res)
